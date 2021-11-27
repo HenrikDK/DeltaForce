64c87293e89a89717de4047cddfd644d7326d634
HenrikDK <commander.john.crichton@gmail.com>
2021-11-27 02:52:51 +0100

    add global usings

---
+++ DeltaForce/GlobalUsings.cs
@@ -0,0 +1,24 @@
+global using System;
+global using System.Data;
+global using System.Data.SqlClient;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using System.Transactions;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+
+global using Dapper;
+global using Lamar;
+global using Lamar.Microsoft.DependencyInjection;
+global using MySql.Data.MySqlClient;
+global using MoreLinq.Extensions;
+global using Npgsql;
+global using Oracle.ManagedDataAccess.Client;
+global using Prometheus;
+
