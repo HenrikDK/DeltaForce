55b74dcab26f0f4edf821193b1a0ff83b5142c34
HenrikDK <commander.john.crichton@gmail.com>
2022-01-09 20:39:43 +0100

    cleanup registry

--- DeltaForce/Infrastructure/WorkerRegistry.cs
+++ DeltaForce/Infrastructure/WorkerRegistry.cs
@@ -4,17 +4,6 @@ public class WorkerRegistry : ServiceRegistry
 {
     public WorkerRegistry()
     {
-        Scan(x =>
-        {
-            x.AssemblyContainingType<Program>();
-                
-            x.WithDefaultConventions();
-
-            x.LookForRegistries();
-                
-            x.ExcludeType<WorkerRegistry>();
-        });
-
         For<IConnectionFactory>().Use<ConnectionFactory>().Singleton();
     }
 }
